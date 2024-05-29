53acbcfc7ac45705c6ad58ccc1d6ac196a554ffa
Kdkplaton <kdkplaton@naver.com>
2024-05-29 14:46:43 +0900

    중간 백업2

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ router.get("/", (req, res) => {
 });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api', [CharactersRouter,ItemsRouter,UsersRouter]);
 app.use(errorHandlerMiddleware);
